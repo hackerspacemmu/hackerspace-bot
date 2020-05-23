2e85498dcbe26a91d9b71430bb1a52e8c7c7aaac
BingYuan <bingyuanng@gmail.com>
2020-05-24 03:20:03 +0800

    Revert "Add script to detect foul words"

--- utils.js
+++ utils.js
@@ -1,16 +1,6 @@
-import { FOUL_WORDS } from './constants.js'
-
 export const load = (commandFiles, client) => {
 	commandFiles.map(async commandFile => {
 		let {default: command} = await import(`./commands/${commandFile}`);
 		client.commands.set(command.name, command);
 	});
 };
-
-export const mind_your_language = (message) => {
-	const { content } = message;
-	const foul = FOUL_WORDS.some(foul_word=>content.toLowerCase().includes(foul_word));
-	if (foul) message.reply('**Language!**');
-
-	return true
-}
