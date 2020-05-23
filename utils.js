b758920eaea87adee1daa58dd320853c54ef17b6
Bing Yuan <bingyuanng@gmail.com>
2020-05-24 04:28:49 +0800

    Change all var to use snake case

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-export const load = (commandFiles, client) => {
-  commandFiles.map(async commandFile => {
-    let {default: command} = await import(`./commands/${commandFile}`);
+export const load = (command_files, client) => {
+  command_files.map(async command_file => {
+    let {default: command} = await import(`./commands/${command_file}`);
     client.commands.set(command.name, command);
   });
 };
