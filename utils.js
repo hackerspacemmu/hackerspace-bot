65ecde81effe6613cdf7ec73338070c374130662
Bing Yuan <bingyuanng@gmail.com>
2020-05-27 01:11:31 +0800

    Fix indentation

--- utils.js
+++ utils.js
@@ -6,29 +6,29 @@ export const load = (command_files, client) => {
 };
 
 export const getUserFromMention = (message, mention) => {
-	if (!mention) return;
+  if (!mention) return;
 
-	if (mention.startsWith('<@') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1);
+  if (mention.startsWith('<@') && mention.endsWith('>')) {
+    mention = mention.slice(2, -1);
 
-		if (mention.startsWith('!')) {
-			mention = mention.slice(1);
-		}
+    if (mention.startsWith('!')) {
+      mention = mention.slice(1);
+    }
     console.log(mention)
-		return message.guild.members.cache.get(mention);
-	}
+    return message.guild.members.cache.get(mention);
+  }
 }
 
 export const getRoleFromMention= (message, mention) => {
-	if (!mention) return;
+  if (!mention) return;
 
-	if (mention.startsWith('<@') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1);
+  if (mention.startsWith('<@') && mention.endsWith('>')) {
+    mention = mention.slice(2, -1);
 
-		if (mention.startsWith('&')) {
-			mention = mention.slice(1);
-		}
+    if (mention.startsWith('&')) {
+      mention = mention.slice(1);
+    }
     console.log(mention)
-		return message.guild.roles.cache.get(mention);
-	}
+    return message.guild.roles.cache.get(mention);
+  }
 }
